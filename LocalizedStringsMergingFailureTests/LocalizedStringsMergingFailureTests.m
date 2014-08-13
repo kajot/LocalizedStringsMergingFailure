a176dbe61ba92c54437ad60fc6947bfe2be97ae3
kajot <kamil.jaworski@gmail.com>
2014-08-13 23:38:55 +0200

    Demo of an Xcode quirk - localizable.strings corrupted every OTHER build.

--- LocalizedStringsMergingFailureTests/LocalizedStringsMergingFailureTests.m
+++ LocalizedStringsMergingFailureTests/LocalizedStringsMergingFailureTests.m
@@ -14,21 +14,41 @@
 
 @implementation LocalizedStringsMergingFailureTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+- (void)testLocalizedStringResolution {
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+    // On XCode 5.1.1 this test passes every OTHER build :)
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    // Found a guy that had the same issue on SO:
+    // http://stackoverflow.com/questions/11481348/localizable-strings-failing-to-load-every-other-build
+
+    // A "localization table" is a set of files
+    //      <locale_name>.lproj/<table_name>.strings
+    //
+    // e.g. a table named "Localizable" (the default name) consist of the following files:
+    //      en.lproj/Localizable.strings
+    //      de.lproj/Localizable.strings
+    //      fr.lproj/Localizable.strings
+
+    // Solution to the problem:
+    // Have only ONE localization table called "Localizable" (default name for the localization table).
+    // In general - do not create more than one table with the same name (even a custom one).
+
+    // In this very project, there are two tables "Localizable". One should rename all the "Localizable.strings"
+    // files under the "Localizations2" folder to "AnotherTableName.strings"
+
+    // You can then query for localized strings from the new table by using
+    //     NSLocalizedStringFromTable(<#key#>, <#tbl#>, <#comment#>)
+    //      tbl - @"AnotherTableName"
+
+    // In general, separating localized strings into separate tables is IMHO a good practice.
+    // See how @mattt does this in his FormatterKit:
+    // https://github.com/mattt/FormatterKit
+    // There is a separate table called "FormatterKit".
+
+    NSString *localizedString = NSLocalizedString(@"testKey1", @"");
+    XCTAssert([localizedString isEqualToString:@"testValue1"]); // this will fail every OTHER build :)
+    // at least on xcode 5.1.1
+
 }
 
 @end
